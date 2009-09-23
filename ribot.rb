16c5a83571f9071ecc591d3189ad216df163c10f
Rob Partington <zimpenfish@gmail.com>
2009-09-23 17:09:59 +0100

    - first steps towards full scribot workings - add database storage via sqlite - uri-find now returns an array with two entries [stringified, raw object] - insert our urls into the database with their metadata - format a nice response to the user

--- ribot.rb
+++ ribot.rb
@@ -3,9 +3,12 @@ require 'xmpp4r'
 require 'xmpp4r/muc/helper/simplemucclient'
 include Jabber
 require 'thread'
-
+require 'dbi'
 require 'uri-find'
 require 'meta-title'
+
+$dbh = DBI.connect('DBI:sqlite3:/home/rjp/.ribot.db', '', '')
+$dbh['AutoCommit'] = false
 
 $bot = nil
 threads = {}
@@ -31,9 +34,9 @@ threads['muc'] = Thread.new {
             print "+ #{msg.from} #{msg.body}\n"
             urls = rule(msg.body, 'http')
             urls.each do |url|
-	            # look for any urls, enqueue them onto q_urls
-                print "MUC enqueuing [#{url}]\n"
-	            q_urls.enq [msg.from, url]
+                # look for any urls, enqueue them onto q_urls
+                print "MUC enqueuing [#{url[0]}]\n"
+                q_urls.enq [msg.from, url]
             end
         end
     end
@@ -47,9 +50,26 @@ threads['meta'] = Thread.new {
         print "MTA waiting for an item\n"
         obj = q_urls.deq
         puts "meta for " + obj.join(', ') + " b=#{$bot}"
-        a = Thread.new { 
-            t = title_from_uri(obj[1])
-            threads['muc']['bot'].say "#{i} #{t}"
+        a = Thread.new {
+            myobj = obj.dup
+            t = title_from_uri(myobj[1][0])
+# CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, url varchar(1024), wh timestamp, user varchar(256), private int, title varchar(1024));
+            last_id = nil
+            $dbh.transaction do
+                $dbh.do(
+                    "INSERT INTO urls (url, wh, user, private, title)
+                     VALUES (?, DATETIME('NOW'), ?, ?, ?)",
+                    myobj[1][0], myobj[0], 0, t
+                )
+                last_id = $dbh.select_one(
+                    "SELECT last_insert_rowid()"
+                )
+            end
+            my_id = last_id[0]
+            domain = obj[1][1].host.split('.')[-3..-1].join('.')
+## 16:24 < scribot> 67041: [www.youtube.com]: vs (YouTube - Maya The Tamperer (Can You Feel It))
+            response = "#{my_id}: [#{domain}]: #{t}"
+            threads['muc']['bot'].say response
         }
         print "MTA incrementing and relooping\n"
         i = i + 1
